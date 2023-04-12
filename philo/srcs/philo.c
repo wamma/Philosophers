554edd8a7b744f6469c273f7f13c91fb2af2ed96
Hyungjun <pparkheong96@naver.com>
2023-04-12 20:36:27 +0900

    implementing philo.c

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,21 +6,32 @@
 /*   By: hyungjup <hyungjup@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:43:50 by hyungjup          #+#    #+#             */
-/*   Updated: 2023/04/11 21:40:06 by hyungjup         ###   ########.fr       */
+/*   Updated: 2023/04/12 20:36:08 by hyungjup         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+int	ft_philo_do(t_philo *philo, t_info *info)
+{
+	pthread_mutex_lock(&(philo->eat));
+	printf("%s", philo->id);
+	philo	
+}
 
 void	*ft_pthread(void *philo)
 {
 	t_philo	*t_philo;
 	t_info	*tmp_philo;
 
-	tmp_philo = (t_info)t_philo;
-	t_philo = tmp_philo->philo;
+	tmp_philo = (t_info *)t_philo;
+	t_philo = &tmp_philo->philo;
 	if (tmp_philo->id % 2)
 		usleep(10000);
+	while (!(philo->die))
+	{
+		if (ft_philo_do(philo, tmp_philo))
+	}
 	return (0);
 }
 
@@ -36,7 +47,7 @@ int	ft_start_philo(t_philo *philo)
 		philo->info[i].check_death_time = ft_time();
 		tmp_philo = (void *)&(philo->info[i]);
 		if (pthread_create(&(philo->info[i].thread_id), NULL, \
-			ft_pthread, tmp_philo))
+					ft_pthread, tmp_philo))
 			return (-1);
 		i++;
 	}
